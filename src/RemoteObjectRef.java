8f86d4205dd6733fde045615a85fdb5def2a3117
Qing Wei <wqggu2013@gmail.com>
2014-06-15 21:35:47 -0400

    add a instance variable to RemoteObjectRef.java RemoteInterfaceName

--- src/RemoteObjectRef.java
+++ src/RemoteObjectRef.java
@@ -1,12 +1,37 @@
+import java.lang.reflect.Constructor;
 
-public class RemoteObjectRef {
+
+public class RemoteObjectRef<T extends StubClass> {
 	String ipAddr;
 	int port;
 	String objectKey;
+	String remoteInterfaceName;
 	
-	public RemoteObjectRef(String ip, int portNum, String key) {
+	public RemoteObjectRef(String ip, int portNum, 
+			String key, String interfaceName) {
 		ipAddr = ip;
 		port = portNum;
 		objectKey = key;
+		remoteInterfaceName = interfaceName;
+	}
+	
+	/**
+	 * Get the stub of remote object
+	 * 
+	 * @return the client-side proxy of remote object(stub)
+	 * @throws ClassNotFoundException
+	 */
+	T localise() throws ClassNotFoundException {
+		Class<T> c = (Class<T>) Class.forName(remoteInterfaceName + "_Stub");
+		Constructor<?> constructor = null;
+		
+		T stub = null;
+		try {
+			constructor = c.getConstructor(new Class[]{RemoteObjectRef.class});
+			stub = (T)constructor.newInstance(this);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return stub;
 	}
 }
