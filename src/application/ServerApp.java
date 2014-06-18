0437ecdaf2cb5c67c7b81d351f3e063e3c7ca47b
Jeremy Fu <gengf@andrew.cmu.edu>
2014-06-18 13:30:43 -0400

    ServerApp exports SayHello.

--- src/application/ServerApp.java
+++ src/application/ServerApp.java
@@ -8,6 +8,7 @@ import registry.LocateRegistry440;
 import registry.Registry440;
 import ror.RemoteObjectRef;
 import ror.UnicastRemoteObject440;
+import example.SayHello;
 import example2.Person;
 
 /**
@@ -17,11 +18,11 @@ public class ServerApp {
 	public static void main(String[] args) throws InterruptedException, UnknownHostException {
 		
 		//SayHello remoteObj = new SayHello();
-		Person person = new Person();
-		RemoteObjectRef ror = UnicastRemoteObject440.exportObject(person, 0);
+		SayHello sayHello = new SayHello();
+		RemoteObjectRef ror = UnicastRemoteObject440.exportObject(sayHello, 0);
 		String host = InetAddress.getLocalHost().getHostName();
 		Registry440 registry = LocateRegistry440.getRegistry(host, 1099);
-		registry.rebind("Person" ,ror);
+		registry.rebind("SayHelloOnRegistry" ,ror);
 		System.out.println("Bind on registry");
 	}
 }
