caa6f443fdb836cc4e28a1bb7a525f5d6774c86b
Qing Wei <wqggu2013@gmail.com>
2014-06-19 18:00:53 -0400

    merge

--- src/test/testNaming.java
+++ src/test/testNaming.java
@@ -7,7 +7,7 @@ import registry.Registry440;
 
 public class testNaming {
 	public static void main(String[] args) throws Exception {
-		Registry440 registry = LocateRegistry440.getRegistry("128.237.217.119", 1099);
+		Registry440 registry = LocateRegistry440.getRegistry("128.237.220.250", 1099);
 		SayHelloInterface sayHello_stub = null;
 		try {
 			sayHello_stub = (SayHelloInterface) registry.lookup("SayHelloOnJeremyRegistry");
@@ -16,8 +16,9 @@ public class testNaming {
 		}
 		PersonInterface person_stub = null;
 		person_stub = (PersonInterface) sayHello_stub.createPerson();
-		person_stub.setName("Kim");
+		person_stub.setName("Kimmmm");
 		String name = person_stub.getName();
+		//System.out.println(name);
 		System.out.println(sayHello_stub.sayHello(person_stub));
 	}
 }
