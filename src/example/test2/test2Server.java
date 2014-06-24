f5c462a293350f459d8494e08c543e527fbcd806
Jeremy Fu <gengf@andrew.cmu.edu>
2014-06-23 21:31:36 -0400

    Add annotations to test2Server.java

--- src/example/test2/test2Server.java
+++ src/example/test2/test2Server.java
@@ -12,11 +12,9 @@ import example.person.PersonInterface;
  * test2: This program tests when parameter and return value of remote method
  * are both non-remote serializable object. 
  * 
- * It looks up the stub of a person object named 'PersonOnServerRegistry', 
- * sets the name of the remote object to the String parameter passed in and 
- * finally gets the name of the remote object again after the name is set. 
- * Both of the setter and getter method of the instance variable NAME are 
- * remote method invocations
+ * Server: It instantiates a Person object, exports it and rebinds it with
+ * the registry. Due to the object is referenced the by proxy dispatcher 
+ * thread, the person object won't be collected by garbage collection.
  * 
  */
 public class test2Server {
