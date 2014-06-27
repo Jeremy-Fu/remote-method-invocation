b593dc12ad42ac1503c7bdf476ee9f1695685f5f
Qing Wei <wqggu2013@gmail.com>
2014-06-27 15:16:32 -0400

    implement Exception packages for remote exception

---
+++ src/exception/RegistryNotFoundException440.java
@@ -0,0 +1,10 @@
+package exception;
+
+public class RegistryNotFoundException440 extends RemoteException440{
+
+	private static final long serialVersionUID = 8773032899916501236L;
+	
+	public RegistryNotFoundException440(String message) {
+		super(message);
+	}
+}
